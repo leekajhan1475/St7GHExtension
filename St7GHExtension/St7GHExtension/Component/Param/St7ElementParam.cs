fbb042e7a109d054e9afff8d6876d0488a56c06f
zonhan1475 <61720983+leekajhan1475@users.noreply.github.com>
2025-06-16 17:48:08 +0800

    Add files via upload

---
+++ St7GHExtension/St7GHExtension/Component/Param/St7ElementParam.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using St7Toolkit.GHExtension.Template;
+using St7Toolkit.GHExtension.Goos;
+
+namespace St7Toolkit.GHExtension.Param
+{
+    /// <summary>
+    /// Represents the elements in Strand7:
+    /// node, beam, plate etc.
+    /// </summary>
+    public class St7ElementParam : St7Param<GH_St7Element>
+    {
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// You can add image files to your project resources and access them like this:
+        /// return Resources.IconForThisComponent;
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon { get; } = null;
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid { get; } = new Guid("4b13db7e-52ca-4d12-9bff-eb83e185f70f");
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override string TypeName { get; } = "Strand7 Element";
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public St7ElementParam() : 
+            base(
+                "St7Element", 
+                "Elem", 
+                "Represents a collection of Strand7 elements",
+                "St7Toolkit.GHExtension", 
+                "Params"
+                )
+        {
+        }
+    }
+
+}
---
+++ St7GHExtension/St7GHExtension/Goos/GH_St7Model.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Grasshopper.Kernel.Types;
+using St7Toolkit.Element;
+using St7Toolkit.GHExtension.Kernel;
+
+namespace St7Toolkit.GHExtension.Goos
+{
+    public class GH_St7Model : GH_Goo<object>
+    {
+
+        /// <summary>
+        /// Override name property
+        /// </summary>
+        public override string TypeName { get; } = "St7Model";
+
+        /// <summary>
+        /// Override description property
+        /// </summary>
+        public override string TypeDescription { get; } = "Represents a Strant7 Model";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid ReferenceID { get; set; } = Guid.Empty;
+
+        /// <summary>
+        /// Get whether Strand7 model is valid 
+        /// </summary>
+        public override bool IsValid 
+        { 
+            get 
+            { 
+                return this.Value != null; 
+            } 
+        }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public GH_St7Model() : base() { }
+
+        /// <summary>
+        /// This function is called when Grasshopper needs to convert this
+        /// instance of <see cref="GH_St7Model"/> into <see cref="St7Model"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="target"></param>
+        /// <returns>
+        /// True if successful, or false on failure.
+        /// </returns>
+        public override bool CastTo<T>(ref T target)
+        {
+            // Check if this.Value object's type is similar
+            // to the type of the target T
+            if (this.Value.GetType() == typeof(T))
+            {
+                object ptr = this.Value;
+                target = (T)ptr;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// This function is called when Grasshopper needs to convert other data into GH_St7Model.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public override bool CastFrom(object source)
+        {
+            // Abort immediately on bogus data.
+            if (source == null) { return false; }
+
+            // Try using the St7Toolkit model converter.
+            // it should Convert since this is a model object
+            St7Model model = new St7Model(-1);
+            if (GH_St7Convert.ToSt7Model(source, ref model))
+            {
+                this.Value = model;
+                return true;
+            }
+
+            // Exhausted the possible conversions,
+            // it seems that input "source" cannot
+            // be converted into a GH_St7Model type.
+            return false;
+        }
+
+        /// <summary>
+        /// Override method
+        /// </summary>
+        /// <returns></returns>
+        public override IGH_Goo Duplicate()
+        {
+            return new GH_St7Element { Value = Value };
+        }
+
+        /// <summary>
+        /// Override ToString method
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return (this.Value == null) ? "null" : Value.ToString();
+        }
+    }
+}
